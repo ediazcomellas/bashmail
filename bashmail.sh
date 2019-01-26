f0c3acea7b543aa1262522a7eb7ae821e63f9bb4
Eduardo Díaz <ediaz@ultreia.es>
2019-01-26 13:10:39 +0100

    Corrected typo at variable name Added option to read local .conf file with precedence to system .conf file

--- bashmail.sh
+++ bashmail.sh
@@ -54,11 +54,15 @@ SYSCONFOVERRIDE=0
 # If there is a system configuration file and we can read it, then source it
 if [ ${SYSCONFOVERRIDE:=0} -eq 0 ]
 then
-  if [ -r /etc/bashmail.conf ]
-  then
-    echo "Reading system variables from /etc/bashmail.conf"
-    . /etc/bashmail.conf
-  fi
+    if [ -r bashmail.conf ]
+    then
+        echo "Reading system variables from bashmail.con"
+        . bashmail.conf
+elif [ -r /etc/bashmail.conf ]
+    then
+        echo "Reading system variables from /etc/bashmail.conf"
+        . /etc/bashmail.conf
+    fi
 fi
 exit 0
 
@@ -71,36 +75,36 @@ DUSESSL=${USESSL:-1}
 #Provide a SMTPPORT, if empty
 if [ -z $SMTPPORT ]
 then
-  case $DUSESSL in
-    0)
-      SMTPPORT=587
-      ;;
-    1)
-      SMTPPORT=465
-      ;;
-    2)
-      SMTPPORT=587
-      OPENSSLOPTIONS="-starttls smtp $OPENSSLOPTIONS "
-      ;;
-    *)
-      echo >&2 "Warning: USESSL has unrecognized value. Defaulting to port 25 and no encryption"
-      SMTPPORT=25
-      ;;
-  esac
+    case $DUSESSL in
+        0)
+            SMTPPORT=587
+            ;;
+        1)
+            SMTPPORT=465
+            ;;
+        2)
+            SMTPPORT=587
+            OPENSSLOPTIONS="-starttls smtp $OPENSSLOPTIONS "
+            ;;
+        *)
+            echo >&2 "Warning: USESSL has unrecognized value. Defaulting to port 25 and no encryption"
+            SMTPPORT=25
+            ;;
+    esac
 fi
 
 if [ $SMTPPORT -eq 25 ]
 then
-  echo >&2 "Warning: using port 25 can be problematic. Consider 587"
+    echo >&2 "Warning: using port 25 can be problematic. Consider 587"
 fi
 
 if [ -z $USEAUTH ]
 then
-  USEAUTH=none
+    USEAUTH=none
 elif [ $USEAUTH != "none" ] && [ $USEAUTH != "login" ] && [ $USEAUTH != "plain" ]
 then
-  echo >&2 "ERROR: Unrecognized value for USEAUTH. Please set it to none, plain or login"
-  exit 1
+    echo >&2 "ERROR: Unrecognized value for USEAUTH. Please set it to none, plain or login"
+    exit 1
 fi
 
 
@@ -114,196 +118,196 @@ hash grep 2>/dev/null || { echo >&2 "ERROR: grep command is required but not ava
 # Check if we have base64 (needed for auth)
 if [ $USEAUTH == "plain" ] || [ $USEAUTH == "login" ]
 then
-  hash base64 2>/dev/null || { echo >&2 "ERROR: base64 command is required but not available. Aborting."; exit 1; }
+    hash base64 2>/dev/null || { echo >&2 "ERROR: base64 command is required but not available. Aborting."; exit 1; }
 fi
 
 #Some other things needed if we use ssl
 if [ $USESSL -eq 1 -o $USESSL -eq 2 ]
 then
-  hash openssl 2>/dev/null || { echo >&2 "ERROR: Encryption required but openssl is not available. Aborting."; exit 1; }
-  hash mkfifo 2>/dev/null || { echo >&2 "ERROR: mkfifo command is required but not available. Aborting."; exit 1; }
-  hash mktemp 2>/dev/null || { echo >&2 "ERROR: mktemp command is required but not available. Aborting."; exit 1; }
+    hash openssl 2>/dev/null || { echo >&2 "ERROR: Encryption required but openssl is not available. Aborting."; exit 1; }
+    hash mkfifo 2>/dev/null || { echo >&2 "ERROR: mkfifo command is required but not available. Aborting."; exit 1; }
+    hash mktemp 2>/dev/null || { echo >&2 "ERROR: mktemp command is required but not available. Aborting."; exit 1; }
 fi
 
 
 
 #Function used to check the SMTP status
 function checkStatus {
-  #echo call: checkStatus $1 $2
-  expect=250
-  code=`echo $1 | cut -d- -f1`
-  line=$2
-  numparams=$#
-  if [ $numparams -eq 3 ] ; then
-    expect="${3}"
-  else
-    if [ $numparams -ne 2 ] ; then
-      echo >&2 "Error on call to checkStatus. Incorrect number of parameters $#"
-      exit 2
+    #echo call: checkStatus $1 $2
+    expect=250
+    code=`echo $1 | cut -d- -f1`
+    line=$2
+    numparams=$#
+    if [ $numparams -eq 3 ] ; then
+        expect="${3}"
+    else
+        if [ $numparams -ne 2 ] ; then
+            echo >&2 "Error on call to checkStatus. Incorrect number of parameters $#"
+            exit 2
+        fi
     fi
-  fi
-  if [ -z $code ]
-  then
-    echo >&2 "Error: was expecting ${expect} but received blank. ${1} ${2}"
-    exit 2
+    if [ -z $code ]
+    then
+        echo >&2 "Error: was expecting ${expect} but received blank. ${1} ${2}"
+        exit 2
 elif [ $code -ne $expect ]
-  then
-    echo >&2  "Error: was expecting ${expect} but received ${1}:  ${2}"
-    exit 2
-  fi
+    then
+        echo >&2  "Error: was expecting ${expect} but received ${1}:  ${2}"
+        exit 2
+    fi
 }
 
 #Where all the magic happens
 bashmail () {
-  from_addr="$1"
-  to_addr="$2"
-  cc_addr="$3"
-  subject="$4"
-  body="$5"
-  #This vars are initialized at the top of the script
-  myhostname=$DHELOHOSTNAME
-  smtpserver=$SMTPSERVER
-  smtpport=$SMTPPORT
-  ssl=$USESSL
-  auth=$USEAUTH
-  username=$LOGIN
-  password=$PASSWORD
-  timeout=$DSMTPTIMEOUT
+    from_addr="$1"
+    to_addr="$2"
+    cc_addr="$3"
+    subject="$4"
+    body="$5"
+    #This vars are initialized at the top of the script
+    myhostname=$DHELOHOSTNAME
+    smtpserver=$SMTPSERVER
+    smtpport=$SMTPPORT
+    ssl=$USESSL
+    auth=$USEAUTH
+    username=$LOGIN
+    password=$PASSWORD
+    timeout=$DSMTPTIMEOUT
 
 
-  #Flag initialization. Dont modify
-  loginsupported=0
-  plainsupported=0
+    #Flag initialization. Dont modify
+    loginsupported=0
+    plainsupported=0
 
 
-  if [ $ssl -eq 1 ] || [ $ssl -eq 2 ]
-  then
-    FIFODIR=`mktemp -d`
-    mkfifo ${FIFODIR}/sto
-    mkfifo ${FIFODIR}/ots
-    #This has to be in background
-    openssl s_client -connect ${smtpserver}:${smtpport} ${OPENSSLOPTIONS} -ign_eof  -quiet -verify_quiet >& ${FIFODIR}/ots < ${FIFODIR}/sto &
-    #This is tricky... dont change order
-    exec 4< ${FIFODIR}/ots
-    exec 3> ${FIFODIR}/sto
-    OUTFD=3
-    INFD=4
-  else
-    exec 3<>/dev/tcp/${smtpserver}/${smtpport}
-    OUTFD=3
-    INFD=3
-  fi
-
-  read -t ${timeout} -u ${INFD} line
-  #If using openssl and connection fails, an error line is captured
-  echo $line | grep -i error  > /dev/null
-  if [ $? -eq 0 ]
-  then
-    echo >&2 "ERROR: Connection to ${smtpserver}:${smtpport} failed"
-    exit 4
-  fi
-
-  #checkStatus "$sts" "$line" 220
-
-  echo "EHLO ${myhostname}" >&${OUTFD}
-
-  read -t ${timeout} -u ${INFD} sts line
-  checkStatus "$sts" "$line"
-
-  SAL=0
-  while [ $SAL -eq 0 ]
-  do
-    #We are going to read all the remainder lines, one at a time
-    #until it timeouts (1sec)
-    read -u ${INFD}  -t 1 linea
-    SAL=$?
-    #Check if auth login is supported
-    echo $linea |grep -i auth | grep -i login > /dev/null
-    if [ $? -eq 0 ]; then
-      loginsupported=1
-    fi
-    echo $linea |grep -i auth | grep -i plain > /dev/null
-    if [ $? -eq 0 ]; then
-      plainsupported=1
-    fi
-  done
-
-  if [ ${auth} == "login" ]
-  then
-    if [ ${loginsupported} -ne 1 ]
+    if [ $ssl -eq 1 ] || [ $ssl -eq 2 ]
     then
-      echo >&2 "ERROR: AUTH LOGIN requested, but not advertised"
-      exit 3
+        FIFODIR=`mktemp -d`
+        mkfifo ${FIFODIR}/sto
+        mkfifo ${FIFODIR}/ots
+        #This has to be in background
+        openssl s_client -connect ${smtpserver}:${smtpport} ${OPENSSLOPTIONS} -ign_eof  -quiet -verify_quiet >& ${FIFODIR}/ots < ${FIFODIR}/sto &
+        #This is tricky... dont change order
+        exec 4< ${FIFODIR}/ots
+        exec 3> ${FIFODIR}/sto
+        OUTFD=3
+        INFD=4
+    else
+        exec 3<>/dev/tcp/${smtpserver}/${smtpport}
+        OUTFD=3
+        INFD=3
     fi
-    #echo "Voy a autenticarme ${sts} ${line}"
-    userB64=`echo -ne ${username} | base64`
-    passB64=`echo -ne ${password} | base64`
-    echo "AUTH LOGIN" >&${OUTFD}
+
+    read -t ${timeout} -u ${INFD} line
+    #If using openssl and connection fails, an error line is captured
+    echo $line | grep -i error  > /dev/null
+    if [ $? -eq 0 ]
+    then
+        echo >&2 "ERROR: Connection to ${smtpserver}:${smtpport} failed"
+        exit 4
+    fi
+
+    #checkStatus "$sts" "$line" 220
+
+    echo "EHLO ${myhostname}" >&${OUTFD}
+
     read -t ${timeout} -u ${INFD} sts line
-    checkStatus "$sts" "$line" 334
-    echo ${userB64} >&${OUTFD}
-    read -t ${timeout} -u ${INFD} sts line
-    checkStatus "$sts" "$line" 334
-    echo ${passB64} >&${OUTFD}
-    read -t ${timeout} -u ${INFD} sts line
-    checkStatus "$sts" "$line" 235
+    checkStatus "$sts" "$line"
+
+    SAL=0
+    while [ $SAL -eq 0 ]
+    do
+        #We are going to read all the remainder lines, one at a time
+        #until it timeouts (1sec)
+        read -u ${INFD}  -t 1 linea
+        SAL=$?
+        #Check if auth login is supported
+        echo $linea |grep -i auth | grep -i login > /dev/null
+        if [ $? -eq 0 ]; then
+            loginsupported=1
+        fi
+        echo $linea |grep -i auth | grep -i plain > /dev/null
+        if [ $? -eq 0 ]; then
+            plainsupported=1
+        fi
+    done
+
+    if [ ${auth} == "login" ]
+    then
+        if [ ${loginsupported} -ne 1 ]
+        then
+            echo >&2 "ERROR: AUTH LOGIN requested, but not advertised"
+            exit 3
+        fi
+        #echo "Voy a autenticarme ${sts} ${line}"
+        userB64=`echo -ne ${username} | base64`
+        passB64=`echo -ne ${password} | base64`
+        echo "AUTH LOGIN" >&${OUTFD}
+        read -t ${timeout} -u ${INFD} sts line
+        checkStatus "$sts" "$line" 334
+        echo ${userB64} >&${OUTFD}
+        read -t ${timeout} -u ${INFD} sts line
+        checkStatus "$sts" "$line" 334
+        echo ${passB64} >&${OUTFD}
+        read -t ${timeout} -u ${INFD} sts line
+        checkStatus "$sts" "$line" 235
 elif [ ${auth} == "plain" ]
-  then
-    if [ ${plainsupported} -ne 1 ]
     then
-      echo >&2 "ERROR: PLAIN LOGIN requested, but not advertised"
-      exit 3
+        if [ ${plainsupported} -ne 1 ]
+        then
+            echo >&2 "ERROR: PLAIN LOGIN requested, but not advertised"
+            exit 3
+        fi
+        #echo "Voy a autenticarme ${sts} ${line}"
+        userpassB64=`echo -ne "\0${username}\0${password}" | base64`
+        echo "AUTH PLAIN ${userpassB64}" >&${OUTFD}
+        read -t ${timeout} -u ${INFD} sts line
+        checkStatus "$sts" "$line" 235
+
     fi
-    #echo "Voy a autenticarme ${sts} ${line}"
-    userpassB64=`echo -ne "\0${username}\0${password}" | base64`
-    echo "AUTH PLAIN ${userpassB64}" >&${OUTFD}
+
+    echo "MAIL FROM: ${from_addr}" >&${OUTFD}
+
     read -t ${timeout} -u ${INFD} sts line
-    checkStatus "$sts" "$line" 235
+    checkStatus "$sts" "$line"
 
-  fi
+    echo "RCPT TO: ${to_addr}" >&${OUTFD}
 
-  echo "MAIL FROM: ${from_addr}" >&${OUTFD}
+    read -t ${timeout} -u ${INFD} sts line
+    checkStatus "$sts" "$line"
 
-  read -t ${timeout} -u ${INFD} sts line
-  checkStatus "$sts" "$line"
+    echo "DATA" >&${OUTFD}
 
-  echo "RCPT TO: ${to_addr}" >&${OUTFD}
+    read -t ${timeout} -u ${INFD} sts line
+    checkStatus "$sts" "$line" 354
 
-  read -t ${timeout} -u ${INFD} sts line
-  checkStatus "$sts" "$line"
+    if [ ! -z "${cc_addr}" ]
+    then
+        echo "Cc: ${cc_addr}" >&${OUTFD}
+    fi
 
-  echo "DATA" >&${OUTFD}
+    echo "Subject: ${subject}" >&${OUTFD}
+    echo "" >&${OUTFD}
+    echo "${body}" >&${OUTFD}
+    echo "." >&${OUTFD}
 
-  read -t ${timeout} -u ${INFD} sts line
-  checkStatus "$sts" "$line" 354
+    read -t ${timeout} -u ${INFD} sts line
+    checkStatus "$sts" "$line"
 
-  if [ ! -z "${cc_addr}" ]
-  then
-    echo "Cc: ${cc_addr}" >&${OUTFD}
-  fi
-
-  echo "Subject: ${subject}" >&${OUTFD}
-  echo "" >&${OUTFD}
-  echo "${body}" >&${OUTFD}
-  echo "." >&${OUTFD}
-
-  read -t ${timeout} -u ${INFD} sts line
-  checkStatus "$sts" "$line"
-
-  #Clean up and go home
-  exec {OUTFD}>&-
-  if [ $ssl -eq 1 -o $ssl -eq 2 ]
-  then
-    exec {INFD}<&-
-    rm ${FIFODIR}/sto ${FIFODIR}/ots
-    rm -d  ${FIFODIR}
-  fi
+    #Clean up and go home
+    exec {OUTFD}>&-
+    if [ $ssl -eq 1 -o $ssl -eq 2 ]
+    then
+        exec {INFD}<&-
+        rm ${FIFODIR}/sto ${FIFODIR}/ots
+        rm -d  ${FIFODIR}
+    fi
 }
 
 #MYEMAILMESSAGEHERE
 #The sintax is: bashmail from to cc subject body
 
-#You can substitute this variables, set them outside of the script or 
+#You can substitute this variables, set them outside of the script or
 #write the command directly below. Comment out the option you don't want
 
 ## OPTION1
@@ -315,22 +319,22 @@ CCADDRESS=${CCADDRESS:-ccaddress@ccreceiversdomain.com}
 SUBJECT=${SUBJECT:-Test mail at `date`}
 if [ -z $BODY ]
 then
-read -r -d '' BODY << BODYDELIMITER
-In a village of La Mancha, the name of which I have no desire to call 
-to mind, there lived not long since one of those gentlemen that keep 
-a lance in the lance-rack, an old buckler, a lean hack, and a greyhound 
+    read -r -d '' BODY << BODYDELIMITER
+In a village of La Mancha, the name of which I have no desire to call
+to mind, there lived not long since one of those gentlemen that keep
+a lance in the lance-rack, an old buckler, a lean hack, and a greyhound
 for coursing.
 
-An olla of rather more beef than mutton, a salad on most nights, scraps 
-on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays, 
+An olla of rather more beef than mutton, a salad on most nights, scraps
+on Saturdays, lentils on Fridays, and a pigeon or so extra on Sundays,
 made away with three-quarters of his income.
 BODYDELIMITER
 fi
 
-bashmail "${ROMADDRESS}" "${TOADDRESS}" "${CCADDRESS}" "${SUBJECT}" "${BODY}"
+bashmail "${FROMADDRESS}" "${TOADDRESS}" "${CCADDRESS}" "${SUBJECT}" "${BODY}"
 
 ## OPTION 2
-## If you wish, just write the email below as shown 
+## If you wish, just write the email below as shown
 #bashmail "fromaddress@mydomain.com" "toaddress@receiversdomain.com" "ccaddress@ccreceiversdomain.com" "test mail at: `date`" "In a village of La Macha
 #the name of which I have no desire to call to mind
 #there lived not long since one of those gentlemen..."
